46f539e809107e3e326ccbbfef9c4daceb058b22
arikhursheed <arikhursheed@gmail.com>
2020-04-08 19:45:38 -0500

    Done thru Q11

--- scripts/Indeed_Project.sql
+++ scripts/Indeed_Project.sql
@@ -6,7 +6,8 @@ from data_analyst_jobs;*/
 /* select company
 	from data_analyst_jobs
 	limit 10;*/
---Answer: ExxonMobile in row 10.
+--Answer: ExxonMob
+
 
 --Question 3:How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
 /*	select count(title)
@@ -34,7 +35,55 @@ from data_analyst_jobs;*/
 --Answer: KS
 
 --Question 7: Select unique job titles from the data_analyst_jobs table. How many are there?
-	/*SELECT COUNT(DISTINCT(title))
-	FROM data_analyst_jobs;*/
+	SELECT COUNT(DISTINCT(title))
+	FROM data_analyst_jobs;
 -- Answer: 881
 
+--Question 8: How many unique job titles are there for California companies?
+
+	SELECT count(DISTINCT title)
+	from data_analyst_jobs
+	WHERE location= 'CA';
+--Answer: 230 
+
+/*Question 9: Find the name of each company and its average star rating for all companies
+			that have more than 5000 reviews across all locations. How many companiesare there with more
+			that 5000 reviews across all locations?*/
+		SELECT company, ROUND(AVG(star_rating),2)
+		FROM data_analyst_jobs
+		where review_count>5000
+		AND company IS NOT NULL
+		GROUP BY company;
+		
+--Answer: 40 across all locations
+
+/*Question 10: Add the code to order the query in #9 from highest to lowest average star rating.
+				Which company with more than 5000 reviews across all locations in the dataset has 
+				the highest star rating? What is that rating?*/
+SELECT company, review_count,avg_rating
+FROM (SELECT company, SUM(review_count) as review_count,ROUND(AVG(star_rating),2) AS avg_rating
+	  FROM data_analyst_jobs
+	  GROUP BY company) as companies
+WHERE review_count>5000
+AND company IS NOT NULL
+ORDER BY avg_rating DESC;
+
+--QUESTION 11: Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+	SELECT count(analyst_job)
+	FROM (SELECT title
+		  FROM data_analyst_jobs
+		  WHERE title ilike '%Analyst%'
+		  GROUP BY title) as analyst_job;
+--Answer: 754
+
+/*QUESTION 12:How many different job titles do not contain either the word ‘Analyst’ 
+			or the word ‘Analytics’? What word do these positions have in common?*/
+
+SELECT count(analyst_job)
+	FROM (SELECT title
+		  FROM data_analyst_jobs
+		  WHERE title ilike '%Analyst%'
+		  GROUP BY title) as analyst_job;
+
+		
+		
