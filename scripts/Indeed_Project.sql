c397d4121b01fad4c2f057f58f2196641e4f2646
arikhursheed <arikhursheed@gmail.com>
2020-04-08 21:35:52 -0500

    Project done

--- scripts/Indeed_Project.sql
+++ scripts/Indeed_Project.sql
@@ -1,37 +1,37 @@
 --Question 1: How many rows are in the data_analyst_jobs table?
-/*select count(*)
-from data_analyst_jobs;*/
+	select count(*)
+	from data_analyst_jobs;
 
 --Question 1:Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
-/* select company
+    select company
 	from data_analyst_jobs
 	limit 10;*/
---Answer: ExxonMob
+--Answer: ExxonMobil
 
 
 --Question 3:How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
-/*	select count(title)
+     select count(title)
 	from data_analyst_jobs
-	where location= 'TN' OR location='KY';*/
+	where location= 'TN' OR location='KY';
 --Answer: 21 posting for TN AND (27) for either TN or KY.
 
 --Question 4: How many postings in Tennessee have a star rating above 4?
-/* SELECT count(title)
+   SELECT count(title)
    FROM data_analyst_jobs
-   WHERE location='TN' AND star_rating>4; */
+   WHERE location='TN' AND star_rating>4;
 --Answer: (3) posting
 
 --Question 5: How many postings in the dataset have a review count between 500 and 1000?
-	/*SELECT count(title)
+	SELECT count(title)
 	FROM data_analyst_jobs
-	WHERE review_count BETWEEN 500 AND 1000;*/
+	WHERE review_count BETWEEN 500 AND 1000;
 --Answer: 151 review count
 
 /*Question 6: Show the average star rating for each state. The output should show the state as state
               and the average rating for the state as avg_rating. Which state shows the highest average rating?*/
-	/*SELECT location AS state, AVG(star_rating) AS avg_rating
+	SELECT location AS state, AVG(star_rating) AS avg_rating
 	FROM data_analyst_jobs
-	GROUP By location;*/
+	GROUP By location;
 --Answer: KS
 
 --Question 7: Select unique job titles from the data_analyst_jobs table. How many are there?
@@ -78,12 +78,21 @@ ORDER BY avg_rating DESC;
 
 /*QUESTION 12:How many different job titles do not contain either the word ‘Analyst’ 
 			or the word ‘Analytics’? What word do these positions have in common?*/
-
-SELECT count(analyst_job)
+			
+--I get 4 result for this query
+SELECT title as not_analyst_jobs
 	FROM (SELECT title
 		  FROM data_analyst_jobs
-		  WHERE title ilike '%Analyst%'
-		  GROUP BY title) as analyst_job;
+		  WHERE title NOT IN (SELECT title FROM data_analyst_jobs WHERE title iLIKE '%Analytics%'OR title iLIKE '%Analyst%' )
+          GROUP BY title) as title;
+-- I get 4 results for this query		 
+	SELECT DISTINCT(title)
+	FROM data_analyst_jobs
+	WHERE title NOT iLIKE '%Analyst%' AND title NOT ILIKE '%Analytics%'
+--Answer: 4 not analytics jobs and tableau is the comman word amnog the positions.
 
-		
+-- i get 4 for this query
+	    SELECT title
+		FROM data_analyst_jobs
+		WHERE title NOT ILIKE '%Analy%';
 		
